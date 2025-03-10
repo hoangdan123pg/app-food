cf416084a9c1b76bab84a899bf50d3f36a9a8884
hoangdan123pg <dannhhe173549@fpt.edu.vn>
2025-03-10 09:13:52 +0700

    test merge

--- src/component/LoginModal.js
+++ src/component/LoginModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
 import Modal from 'react-native-modal';
 export default function LoginModal({ isVisible, onClose }) {
     return (
@@ -11,15 +11,30 @@ export default function LoginModal({ isVisible, onClose }) {
         style={styles.modal}
       >
         <View style={styles.container}>
+          <ImageBackground 
+            source={require('../assets/login_bg.jpg')} // Đường dẫn ảnh
+            style={styles.backgroundImage}
+            resizeMode="cover"
+          >
           <Text style={styles.title}>Đăng Nhập</Text>
           <Button title="Đóng" onPress={onClose} />
+          </ImageBackground>
         </View>
       </Modal>
     );
   }
   
   const styles = StyleSheet.create({
-    modal: { justifyContent: 'flex-end', margin: 0 },
-    container: { backgroundColor: 'white', padding: 20, borderTopLeftRadius: 20, borderTopRightRadius: 20 },
+    modal: { 
+      justifyContent: 'flex-end',
+      margin: 0 
+    },
+    container: { 
+      
+      flex:1,
+      backgroundColor: 'white', 
+      padding: 20, 
+      borderTopLeftRadius: 10, 
+      borderTopRightRadius: 10 },
     title: { fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginBottom: 10 }
   });
